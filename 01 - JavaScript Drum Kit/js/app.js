4006d4ad65f8b6e10becd3e940b2dcf157c23fd2
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-10-31 14:53:02 +0100

    Day 2

--- 01 - JavaScript Drum Kit/js/app.js
+++ 01 - JavaScript Drum Kit/js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
@@ -7,7 +7,7 @@ window.addEventListener("keydown", function (e) {
     audio.play();
     key.classList.add("playing");
 
-});
+};
 
 function removeTransition(e) {
     if (e.propertyName !== "transform") return;
@@ -18,7 +18,7 @@ const keys = document.querySelectorAll(".key");
 
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
-
+window.addEventListener("keydown", playSound);
 
 
 
